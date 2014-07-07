415ff6a0d7ff4e5bb48d3b6acc83976cbc032127
tsean <tsean>
2014-07-07 19:40:22 +0800

    addZoom

--- Imaginarium/ImageViewController.m
+++ Imaginarium/ImageViewController.m
@@ -8,11 +8,12 @@
 
 #import "ImageViewController.h"
 
-@interface ImageViewController ()
+@interface ImageViewController () <UIScrollViewDelegate>
 
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIImage *image;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -20,7 +21,38 @@
 
 - (void)setScrollView:(UIScrollView *)scrollView {
     _scrollView = scrollView;
-    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    //self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage {
+    self.image = nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request
+                                                        completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+                                                            if (!error) {
+                                                                if ([request.URL isEqual:self.imageURL]) {
+                                                                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                                                                    dispatch_async(dispatch_get_main_queue(), ^{
+                                                                        self.image = image;
+                                                                    });
+                                                                }
+                                                            }
+                                                        }];
+
+        [task resume];
+    }
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
 }
 
 - (void)setImageURL:(NSURL *)imageURL {
@@ -43,6 +75,7 @@
     self.imageView.image = image;
     [self.imageView sizeToFit];
     self.scrollView.contentSize = self.image ?self.image.size : CGSizeZero;
+    [self.spinner stopAnimating];
 }
 
 - (void)viewDidLoad {
